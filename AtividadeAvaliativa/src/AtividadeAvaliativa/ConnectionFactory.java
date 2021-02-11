acd63da0f3780a4d71f74c2a9545ce68baa66579
chiodiniLuan1202 <luangabriel111@hotmail.com>
2021-02-11 20:14:14 -0300

    AtividadeAvaliativa

---
+++ AtividadeAvaliativa/src/AtividadeAvaliativa/ConnectionFactory.java
@@ -0,0 +1,21 @@
+package AtividadeAvaliativa;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+	Connection conn = new ConnectionFactory().getConnection();
+
+	public Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "a");
+			System.out.println("Conectouuuuu!!!");
+		} catch (Exception e) {
+			System.out.println("Deu ruim: " + e.getMessage());
+		}
+		return conn;
+	}
+
+}
--- AtividadeAvaliativa/src/AtividadeAvaliativa/Main.java
+++ AtividadeAvaliativa/src/AtividadeAvaliativa/Main.java
@@ -1,103 +1,53 @@
 package AtividadeAvaliativa;
 
-import java.util.ArrayList;
 import java.util.Scanner;
-
-
 
 public class Main {
 
-	
-		
-		 
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
-		AuxilioEmergencial a= new AuxilioEmergencial();
-		int escolha=0;
-		int desempregado=0;
-			
-			do {
+		AuxilioEmergencial a = new AuxilioEmergencial();
+		int escolha = 0;
 
+		while(escolha != 7) {
 			System.out.println("Menu");
 			System.out.println("1 - Cadastrar nova pessoa");
 			System.out.println("2 - Listar Benefeciados");
 			System.out.println("3 - Maior Tempo");
 			System.out.println("4 - Maior valor");
 			System.out.println("5 - Contar Pessoas");
-			System.out.println("6 - Sair");
+			System.out.println("6 - Monstrar Usuario");
+			System.out.println("7 - Sair");
+			
 			escolha = teclado.nextInt();
-		
-		
+			
 			switch (escolha) {
 			case 1:
-				teclado.nextLine();
-				System.out.println("Digite seu nome completo:");
-				String nome= teclado.nextLine();
-				System.out.println("Digite sua situção entre: 1-EMPREGADO,2-DESEMPREGADO,3-EMPREGADOR");
-				int opcao = teclado.nextInt();
-				teclado.nextLine();
-				Categoria categoria = null;
-				if(opcao==1) {
-					categoria=categoria.EMPREGADO;
-				}
-				if(opcao==2) { 
-					categoria=categoria.DESEMPREGADO;
-					System.out.println("A quanto tempo esta desempregado ?");
-					desempregado= teclado.nextInt();
-				}
-				if(opcao==3) { 
-					categoria=categoria.EMPREGADOR;
-				}
-				teclado.nextLine();
-				System.out.println("Digite sua data de nascimento:");
-				String dataNasc = teclado.nextLine();
-				System.out.println("Informe sua Idade");
-				int idade = teclado.nextInt();
-				teclado.nextLine();
-				System.out.println("Informe seu Estado: ");
-				String estado = teclado.nextLine().trim();
-				System.out.println("É aposentado 1- sim e 2- não ?");
-				int apos=teclado.nextInt();
-				boolean aposentado;
-				teclado.nextLine();
-				if(apos==1) {
-					aposentado=true;
-				}else {
-					aposentado=false;
-				}
-				
-				Beneficiados p1=new Beneficiados(nome,idade,dataNasc,categoria,estado,aposentado,desempregado);
-				if(p1.getEstado()=="SAO PAULO") {
-					p1.moraSaoPaulo();
-				}
-				
-				if(p1.getEstado()=="PARA") {
-					p1.moraSaoPaulo();
-				}
-				a.addBeneficiados(p1);
+				a.insertBeneficiado();
 				break;
 			case 2:
-				System.out.println(a.listarBeneficiados());
-			
+				a.listarCadastros();
 				break;
 			case 3:
-				a.maiorTempo();
-			
+				a.listarMaiorTempo();
 				break;
 			case 4:
-				a.maiorValor();
-			
+				a.listarMaiorValor();
 				break;
-				case 5:
-					System.out.println("Quantidade de pessoa cadastradas");
-					System.out.println(a.contarElementos());
+			case 5:
+				a.listarTotalBeneficiarios();
+				break;
 				
-					break;
+			case 6:
+				a.mostrarBeneficiado();;
+				break;	
 			}
-			}while(escolha!=6);
-	
+		}
+		
+		teclado.close();
+		
 	}
-	
+
 	
 
 }
